4aacd7c8daabd68c4994d35b0deb78e7d598a97f
Ahmad Omar <ahmad@pop-os.localdomain>
2021-09-15 12:33:23 +0300

    fetch and display data Relates #7

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import './Content.css'
-import image from '../assets/image.jpg'
-function Content ({content, handleContent}) {
+import image from '../assets/image.jpeg'
+function Content ({contents, handleContent}) {
   return (
     <main className="main-content">
       <section className="news-cards">
-        <div className="card">
-          <div className="card-image">
-            <img src={image} alt="news" />
-          </div>
-          <div className="card-content">
-            <h3 className="card-title">Title</h3>
-            <p className="card-paragraph">paragraph</p>
-            <span className="card-keywords">keywords</span>
-          </div>
-        </div>
+          {contents.map((content) => {
+            const {abstract, lead_paragraph, multimedia, keywords } = content
+            return (<div key={Math.random()*10} className="card">
+            <div className="card-image">
+              {multimedia.length !== 0 ? <img src={`https://www.nytimes.com/${multimedia[0].url}`} alt="news" /> : <img src={image} alt="news" />}
+            </div>
+            <div className="card-content">
+              <h3 className="card-title">{abstract}</h3>
+              <p className="card-paragraph">{lead_paragraph}</p>
+              {keywords.length !== 0 ? <span className="card-keywords">{keywords[0].value}</span> : ''}
+            </div>
+          </div>)
+            
+          })}
+        
       </section>
     </main>
   )
